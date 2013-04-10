7f139221d889db4b3c2efbe3d2121866ae55a022
Alex Ford <Alex@CodeTunnel.com>
2013-04-10 17:43:19 -0600

    Refactored help command to do better spacing calculation.

--- defaultCmds/help.js
+++ defaultCmds/help.js
@@ -11,13 +11,14 @@ exports.options = {
 
 exports.invoke = function (res, options, shell) {
     if (!options.command) {
+        var maxLength = 0;
+        for (var key in shell.cmds)
+            maxLength = key.length > maxLength ? key.length : maxLength;
         for (var key in shell.cmds) {
             var cmd = shell.cmds[key],
                 helpStr = key;
             if (cmd.description) {
-                for (var count = 0; count < (20 - key.length); count++) {
-                    helpStr += ' ';
-                }
+                helpStr += new Array((maxLength - key.length) + 10).join(' ');
                 helpStr += cmd.description;
             }
             res.log(helpStr);
@@ -37,18 +38,24 @@ exports.invoke = function (res, options, shell) {
                 res.log();
             }
             if (cmd.options) {
+                var strs = [],
+                    maxLength = 0;
                 for (var key in cmd.options) {
-                    var option = cmd.options[key], optionStr = (key.length > 1 ? '--' : '-') + key;
+                    var option = cmd.options[key],
+                        optionStr = (key.length > 1 ? '--' : '-') + key;
                     if (option.aliases) {
                         option.aliases.forEach(function (alias) {
-                            optionStr += ', ' + (alias.length > 1 ? '--' : '-') + alias;
+                            optionStr += ',' + (alias.length > 1 ? '--' : '-') + alias;
                         });
                     }
+                    strs.push({ option: option, str: optionStr});
+                    maxLength = optionStr.length > maxLength ? optionStr.length : maxLength;
+                }
+                for (var index in strs) {
+                    var option = strs[index].option,
+                        optionStr = strs[index].str;
                     if (option.description) {
-                        var length = optionStr.length;
-                        for (var count = 0; count < (20 - length); count++) {
-                            optionStr += ' ';
-                        }
+                        optionStr += new Array((maxLength - optionStr.length) + 10).join(' ');
                         optionStr += option.description;
                     }
                     res.log(optionStr);
