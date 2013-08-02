bf49e7a8304405e3173a66cf6e5ffb069682d367
Alex Ford <Alex.Ford@CodeTunnel.com>
2013-08-02 17:41:40 -0600

    Updating app to work with built in readline module instead of prompt.

--- example/app.js
+++ example/app.js
@@ -1,4 +1,4 @@
-var prompt = require('prompt'),
+var readline = require('readline'),
     shotgun = require('../index'),
     shell = new shotgun.Shell({
         defaultCmds: {
@@ -6,27 +6,26 @@ var prompt = require('prompt'),
         }
     }),
     context = {};
-function callback(err, val) {
-    var result = {}, exit = false;
-    if (!err && val.cmdStr) {
-        result = shell.execute(val.cmdStr, context);
 
-        context = result.context;
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-        if (result.clearDisplay) {
-            for (var i = 0; i < 50; i++) {
-                console.log('\r\n');
-            }
-        }
-
-        exit = result.exit;
-
-        result.lines.forEach(function (line) {
-            console[line.type](line.text);
-        });
-    }
-    if (!exit) {
-        prompt.get('cmdStr', callback);
-    }
+function parseInput(cmdStr) {
+    var result = shell.execute(cmdStr, context);
+    context = result.context;
+    if (result.clearDisplay)
+        for (var i = 0; i < 50; i++)
+            console.log('\r\n');
+    result.lines.forEach(function (line) {
+        console[line.type](line.text);
+    });
+    exit = result.exit;
+    if (result.exit)
+        rl.close();
+    else
+        rl.question("> ", parseInput);
 }
-prompt.get('cmdStr', callback);
+
+rl.question("> ", parseInput);
