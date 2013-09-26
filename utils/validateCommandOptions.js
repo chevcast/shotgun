9c8385940645d89d99d47ad6f732dd89549ad955
Alex Ford <alex.ford@codetunnel.com>
2013-09-26 13:18:32 -0600

    Added shell to the arguments passed to command module validate functions.

--- utils/validateCommandOptions.js
+++ utils/validateCommandOptions.js
@@ -64,7 +64,7 @@ module.exports = exports = function (options, cmd, shell) {
                     try {
                         // If the validation function returns false then do not invoke the command and write
                         // error message.
-                        var validationResult = definedOption.validate(options[key], options);
+                        var validationResult = definedOption.validate(options[key], options, shell);
                         if (validationResult !== true) {
                             if (typeof(validationResult) !== 'string')
                                 shell.error('invalid value for "' + key + '"');
