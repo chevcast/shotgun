5b091bff8fdf759867327ae7a3a3a6163b3a0d5e
Alex Ford <Alex.Ford@CodeTunnel.com>
2014-02-26 18:36:46 -0700

    Updating example file to include helpful comments.

--- example/app.js
+++ example/app.js
@@ -1,36 +1,56 @@
 var readline = require('readline'),
     shotgun = require('../index'),
+    // Create a new shotgun shell instance.
     shell = new shotgun.Shell({ debug: true }),
+    // Store the currently active user.
     currentUser = "guest",
+    // Store each user's context data here.
     contexts = {
         guest: {},
         joe: {}
     };
 
+// Use node's core readline library to create a looping prompt for user input.
 var rl = readline.createInterface(process.stdin, process.stdout);
+// Set the prompt string.
 rl.setPrompt(currentUser + '> ');
 
 shell
-    .on('switchUser', function (username, contextData) {
-        contexts[currentUser] = contextData;
+    // We expect to receive the new username and the contextData for the current
+    // user so we can store it for later.
+    .on('switchUser', function (username, currentUserContext) {
+        contexts[currentUser] = currentUserContext;
         currentUser = username;
         shell.log("User switched to: " + currentUser);
+        
+        // Set prompt string so it includes the active username.
         rl.setPrompt(currentUser + '> ');
     })
+    // When the log event occurs just log the text to the console.
+    // What you do with the text here depends on the type of application you are
+    // building.
     .on('log', function (text, options) {
-        console[options.type](text);
+        console.log(text);
     })
+    // When the clear event occurs clear the console with these ASCII control
+    // sequences. http://stackoverflow.com/questions/8813142/clear-terminal-window-in-node-js-readline-shell
     .on('clear', function () {
         console.log('\u001B[2J\u001B[0;0f');
     })
+    // When the exit event occurs, kill the readline prompt and then kill the process.
     .on('exit', function () {
         rl.close();
         process.exit();
     })
+    // If any errors occur then send them straight to stderr.
     .on('error', console.error.bind(console));
 
 rl.on('line', function (userInput) {
+    // On each user input, pass the input straight into your shell
+    // and let shotgun work its magic.
     shell.execute(userInput, contexts[currentUser]);
+
+    // Resume the readline prompt.
     rl.prompt();
 });
 
